bd32bdc2ac9997bafa81f5315dcbeb5ba0a69c68
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2024-03-29 18:57:44 -0300

    feat: finish second exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# 1º) Faça um Programa que leia uma lista de 10 caracteres, e diga quantas consoantes foram lidas.
-# Imprima as consoantes
+""" 1º) Faça um Programa que leia uma lista de 10 caracteres, e diga quantas consoantes foram lidas.
+Imprima as consoantes """
 
 def exercise01():
   vowels = ['a', 'e', 'i', 'o', 'u']
@@ -12,4 +12,38 @@ def exercise01():
 
   print(f'Consoantes: {consonants}')
 
-exercise01()
+""" 2º) Faça um programa que receba a temperatura média de cada mês do ano e armazene-as em uma
+lista. Após isto, calcule a média anual das temperaturas e mostre todas as temperaturas acima da
+média anual, e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . .).  """
+
+def exercise02():
+  yearTemperature = []
+  monthTemperatureHigherThanAverage = {}
+  mappedMonthByIndex = {
+    '0': 'Janeiro',
+    '1': 'Fevereiro',
+    '2': 'Março',
+    '3': 'Abril',
+    '4': 'Maio',
+    '5': 'Junho',
+    '6': 'Julho',
+    '7': 'Agosto',
+    '8': 'Setembro',
+    '9': 'Outubro',
+    '10': 'Novembro',
+    '11': 'Dezembro'
+  }
+
+  for i in range(12):
+    yearTemperature.append(float(input(f'Digite a temperatura média do mês {i+1} em graus celsius: ')))
+  
+  averageTemperature = sum(yearTemperature) / len(yearTemperature)
+  
+  for i, temperature in enumerate(yearTemperature):
+    if temperature > averageTemperature:
+      monthTemperatureHigherThanAverage[i] = temperature
+  
+  print(f'Temperaturas acima da média anual: ')
+
+  for monthIndex, temperature in monthTemperatureHigherThanAverage.items():
+    print(f'{mappedMonthByIndex[str(monthIndex+1)]} - {temperature}°C')
