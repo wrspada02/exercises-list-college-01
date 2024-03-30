4b76f020d1b08e62335f0fa923a96d23ff74db94
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2024-03-29 21:31:20 -0300

    fix: return mapped month dicionary to starting from 1 to 12

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ def exercise02():
   yearTemperature = []
   monthTemperatureHigherThanAverage = {}
   mappedMonthByIndex = {
-    '0': 'Janeiro',
-    '1': 'Fevereiro',
-    '2': 'Março',
-    '3': 'Abril',
-    '4': 'Maio',
-    '5': 'Junho',
-    '6': 'Julho',
-    '7': 'Agosto',
-    '8': 'Setembro',
-    '9': 'Outubro',
-    '10': 'Novembro',
-    '11': 'Dezembro'
+    '1': 'Janeiro',
+    '2': 'Fevereiro',
+    '3': 'Março',
+    '4': 'Abril',
+    '5': 'Maio',
+    '6': 'Junho',
+    '7': 'Julho',
+    '8': 'Agosto',
+    '9': 'Setembro',
+    '10': 'Outubro',
+    '11': 'Novembro',
+    '12': 'Dezembro'
   }
 
   for i in range(12):
