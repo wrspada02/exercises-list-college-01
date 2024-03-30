64e0c9723cb12e40f49b96532ad37cf9c9ea4037
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2024-03-29 21:24:48 -0300

    feat: finished ninety exercise

--- main.py
+++ main.py
@@ -276,3 +276,71 @@ def exercise08():
 
   for key, value in salaryRanges.items():
     print(f'{key}: {value}')
+
+""" 9º) Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para
+saber qual o melhor jogador após cada jogo. Para isto, faz-se necessário o desenvolvimento de um
+programa, que será utilizado pelas telefonistas, para a computação dos votos. Sua equipe foi
+contratada para desenvolver este programa, utilizando a linguagem de programação Python. Para
+computar cada voto, a telefonista digitará um número, entre 1 e 23, correspondente ao número da
+camisa do jogador. Um número de jogador igual zero, indica que a votação foi encerrada. Se um
+número inválido for digitado, o programa deve ignorá-lo, mostrando uma breve mensagem de aviso, e
+voltando a pedir outro número. Após o final da votação, o programa deverá exibir:
+1. O total de votos computados;
+2. Os números e respectivos votos de todos os jogadores que receberam votos;
+3. O percentual de votos de cada um destes jogadores;
+4. O número do jogador escolhido como o melhor jogador da partida, juntamente com o número
+de votos e o percentual de votos dados a ele.
+ Observe que os votos inválidos e o zero final não devem ser computados como votos. O
+resultado aparece ordenado pelo número do jogador. O programa deve fazer uso de arrays. O
+programa deverá executar o cálculo do percentual de cada jogador através de uma função.
+Esta função receberá dois parâmetros: o número de votos de um jogador e o total de votos. A
+função calculará o percentual e retornará o valor calculado. Abaixo segue uma tela de
+exemplo. A disposição das informações deve ser o mais próxima possível ao exemplo. Os dados
+são fictícios e podem mudar a cada execução do programa. Ao final, o programa deve ainda
+gravar os dados referentes ao resultado da votação em um arquivo texto no disco, obedecendo
+a mesma disposição apresentada na tela.
+Enquete: Quem foi o melhor jogador?
+Número do jogador (0=fim): 9
+Número do jogador (0=fim): 10
+Número do jogador (0=fim): 9
+Número do jogador (0=fim): 10
+Número do jogador (0=fim): 11
+Número do jogador (0=fim): 10
+Número do jogador (0=fim): 50
+Informe um valor entre 1 e 23 ou 0 para sair!
+Número do jogador (0=fim): 9
+Número do jogador (0=fim): 9
+Número do jogador (0=fim): 0
+Resultado da votação:
+Foram computados 8 votos.
+Jogador Votos %
+9 4 50,0%
+10 3 37,5%
+11 1 12,5%
+O melhor jogador foi o número 9, com 4 votos, correspondendo a 50% do total de votos. """
+
+def exercise09():
+  def calcPercentage(value, total):
+    return value / total * 100
+
+  players = [0] * 23
+  player = 1
+
+  while player != 0:
+    player = int(input('Número do jogador (0=fim): '))
+    if player != 0:
+      if player >= 1 and player <= 23:
+        players[player-1] += 1
+      else:
+        print('Informe um valor entre 1 e 23 ou 0 para sair!')
+  
+  totalVotes = sum(players)
+
+  print('Resultado da votação:')
+  print(f'Foram computados {totalVotes} votos.')
+
+  for i, playerVotes in enumerate(players):
+    if playerVotes > 0:
+      print(f'Jogador {i+1} {playerVotes} {round(calcPercentage(playerVotes, totalVotes), 2)}%')
+
+  print(f'O melhor jogador foi o número {players.index(max(players)) + 1}, com {max(players)} votos, correspondendo a {round(calcPercentage(max(players), totalVotes), 2)}% do total de votos.')
